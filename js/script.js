c1672d065da68d7e29671f1edefe5b6f03fc3f4d
joy_03 <85474393+whdldi@users.noreply.github.com>
2022-01-23 23:57:37 +0900

    +) card modal script
    멤별 카드 추가해야함 (1명만 진행중)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-      /* 네비메뉴 호버시나오게 */
   $(function(){
       $('header nav ul.gnb .lymenu_sub1').hover(function(){
             $('header nav ul.sub1').stop().slideToggle();
@@ -16,20 +15,13 @@ $(document).ready(function(){
 
 
   $(function(){
-  //1. header가 가지고 있는 위치값 = 0 > offset().top 
-  //2. 스크롤을 했을때 스크롤바의 수직값 = 0 > $(window).scrollTop() 
-  //스크롤을 했을때 hedaer가 가지고 있는 값(0)보다 스크롤바의 수직값이 더 클때 스타일 적용
-  /* 이하-헤더 탑 이라는 그릇에 헤더의 탑값을 오프셋으로 담아냄 */
   var headerTop = $('header').offset().top;
   $(window).scroll(function(){
         var scrollTop = $(window).scrollTop();
-        // console.log(headerTop);
-        // console.log(scrollTop);
         if(headerTop < scrollTop){
-            //헤더 탑 값보다 스크롤했을때 값이 더 클때(내리니까 값이 커질수밖에 없음) 스크립트 코드작성(클래스명 fixed가 붙음)
-            $('header').addClass('fixed'); //헤더가 고정된다
+            $('header').addClass('fixed'); 
         } else {
-            $('header').removeClass('fixed'); //값이 크지않을때(=원래자리에 있을때는 fixed 클래스가 없어진다 즉 색이랑 효과가 원래대로 돌아온다)
+            $('header').removeClass('fixed');
         }
   });
   });
@@ -75,21 +67,40 @@ $(document).ready(function(){
 
       /* 헤더고정 스크롤 했을때 */
       $(function(){
-        /* headertop이라는 그릇에 header오프셋탑값을넣음 */
         var headerTop = $('header').offset().top;
         $(window).scroll(function(){
              var scrollTop = $(window).scrollTop();
-             //console.log(headerTop);
-             //console.log(scrollTop);
              if(headerTop < scrollTop){
-                  //헤더 탑 값보다 스크롤했을때 값이 더 클때(내리니까 값이 커질수밖에 없음) 스크립트 코드작성(클래스명 fixed가 붙음)
-                  $('header').addClass('fixed').stop(); //헤더가 고정
+                  $('header').addClass('fixed').stop(); 
              } else {
-                  $('header').removeClass('fixed'); //값이 크지않을때(=원래자리에 있을때는 fixed 클래스가 없어진다 css에서 .fixed에 효과를 넣어뒀어서 클래스명이 사라지며 색이랑 효과도 원래대로 돌아옴)
+                  $('header').removeClass('fixed'); 
              }
         });
       })
       
+
+/* 이하 카드 모달 */
+    $(function(){
+      // 	이미지 클릭시 해당 이미지 모달
+        $(".jiu").click(function(){
+          $(".modal").fadeIn();
+        });
+        
+        //.modal안에 button을 클릭하면 .modal닫기
+        $(".modal button").click(function(){
+          $(".modal").fadeOut();
+        });
+        
+        //.modal밖에 클릭시 닫힘
+        $(".modal").click(function (e) {
+          if (e.target.className != "modal") {
+            return false;
+          } else {
+            $(".modal").fadeOut();
+          }
+        });
+      });
+
 
 
 });
